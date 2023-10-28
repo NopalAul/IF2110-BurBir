8084f2926c64e0db8a131c49567c2aa869dcd622
NopalAul <nopalaul@LAPTOP-K39U03NT>
2023-10-28 12:00:26 +0700

    gajadi

--- lib/commandList/commandList.h
+++ lib/commandList/commandList.h
@@ -2,6 +2,9 @@
 #define commandList_h
 
 #define DAFTAR "DAFTAR"
-
+#define MASUK "MASUK"
+#define KELUAR "KELUAR"
+#define TUTUP_PROGRAM "TUTUP_PROGRAM"
+#define GANTI_PROFIL "GANTI_PROFIL"
 
 #endif
